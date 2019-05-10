e3c0c37e996c6b967f2451a37ab10351a7be6d7f
munasuke <34709203+munasuke@users.noreply.github.com>
2019-05-10 14:54:56 +0900

    いったん

--- Lib/Lib/main.cpp
+++ Lib/Lib/main.cpp
@@ -4,22 +4,21 @@
 int main()
 {
 	MyLib lib(640);
-	Primitive p(PrimitiveType::box);
-	p.pos[0] = Vec2f(0.0f, 0.0f);
-	p.pos[1] = Vec2f(100.0f, 0.0f);
-	p.pos[2] = Vec2f(0.0f, 100.0f);
-	p.pos[3] = Vec2f(100.0f, 100.0f);
-	Vec3f color = Vec3f(1.0f, 0.0f, 0.0f);
+	//Primitive p(PrimitiveType::box);
+	//p.pos[0] = Vec2f(0.0f, 0.0f);
+	//p.pos[1] = Vec2f(100.0f, 0.0f);
+	//p.pos[2] = Vec2f(0.0f, 100.0f);
+	//p.pos[3] = Vec2f(100.0f, 100.0f);
+	//Vec3f color = Vec3f(1.0f, 0.0f, 0.0f);
 
 	Texture tex("Image/brides.png");
-	tex.size = Vec2f(float(lib.GetWinSize().x), float(lib.GetWinSize().y) / 2);
 
-	CFPSCounter fps(10);
+	//CFPSCounter fps(10);
 
-	while (lib.CheckMsg())
+	while (lib.CheckMsg() && !Input::Get().IsKey(Key::Escape))
 	{
 		lib.Clear();
-		lib.Draw(p, color);
+		//lib.Draw(p, color);
 		lib.Draw(tex);
 		lib.Execution();
 
@@ -27,11 +26,11 @@ int main()
 
 		if (Input::Get().IsKey(Key::Num4))
 		{
-			tex.pos.x--;
+			tex.pos.x-=2.0f;
 		}
 		if (Input::Get().IsKey(Key::Num6))
 		{
-			tex.pos.x++;
+			tex.pos.x+=2.0f;
 		}
 
 		//tex.pos.y++;
